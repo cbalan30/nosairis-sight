d556829af7299ae47a0bcb21484d580ff5a36d03
Siva Balan <siva@stronium.com>
2025-12-15 17:37:46 +0800

    Admin LTE template configured

--- sightproject/core/views.py
+++ sightproject/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'core/main_dashboard.html', {})
+    # return render(request, 'adminlte/login.html', {})
